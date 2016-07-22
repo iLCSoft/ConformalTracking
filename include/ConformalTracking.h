0c822ea1d77b45981d279c6914120ac2d732b3f9
Daniel Hynds <daniel.hynds@cern.ch>
2016-07-22 12:12:22 +0000

    added first version of new algorithms to replace old code

--- include/ConformalTracking.h
+++ include/ConformalTracking.h
@@ -70,6 +70,7 @@ public:
 
   // Track finding
   std::vector<cellularTrack> createTracks(Cell*, std::map<Cell*, bool>&);
+  std::vector<cellularTrack> createTracksNew(Cell*, std::map<Cell*, bool>&);
   bool                       toBeUpdated(std::vector<cellularTrack>);
   void                       followPath(std::vector<cellularTrack>&, int, std::map<Cell*, bool>&, int&);
   void                       updateCell(Cell*);
@@ -78,8 +79,10 @@ public:
   std::vector<KDTrack> getFittedTracks(std::vector<cellularTrack>&, std::vector<double>&);
   std::vector<KDTrack> getLowestChi2(std::vector<KDTrack>, std::vector<double>, std::vector<double>&);
 
+  double chi2SZ(KDTrack);
+
   double fitWithoutPoint(KDTrack, int);
-  bool   sameTrack(KDTrack, KDTrack);
+  int    overlappingHits(KDTrack, KDTrack);
 
   void   extendTrack(KDTrack&, std::vector<cellularTrack>, std::map<KDCluster*, bool>&);
   double fitWithPoint(KDTrack, KDCluster*);
@@ -89,7 +92,7 @@ public:
   ROOT::Minuit2::Minuit2Minimizer newFitter;
 
   // MC truth debug
-  double checkReal(cellularTrack, std::map<KDCluster*, MCParticle*>, std::map<MCParticle*, bool>&);
+  double checkReal(KDTrack, std::map<KDCluster*, MCParticle*>, std::map<MCParticle*, bool>&);
   int    getUniqueHits(std::vector<KDCluster*>);
 
 protected:
@@ -142,13 +145,19 @@ protected:
   TCanvas* m_canvConformalEventDisplayMC;
   TCanvas* m_canvConformalEventDisplayMCunreconstructed;
 
+  TH2F* m_szDistribution;
+  TH2F* m_uvDistribution;
+  TH2F* m_xyDistribution;
+  TH3F* m_xyzDistribution;
+
   // Other constants
-  double m_thetaRange;
-  double m_chi2cut;
-  double m_maxCellAngle;
-  double m_maxDistance;
-  int    m_minClustersOnTrack;
-  bool   m_debugPlots;
+  double     m_thetaRange;
+  double     m_chi2cut;
+  double     m_maxCellAngle;
+  double     m_maxDistance;
+  int        m_minClustersOnTrack;
+  bool       m_debugPlots;
+  KDCluster* debugSeed;
 };
 
 bool sort_by_radius(EVENT::TrackerHit*, EVENT::TrackerHit*);
