3c7d30628f6addbfa75ca5910087d03c5fc95b55
Daniel Hynds <daniel.hynds@cern.ch>
2017-04-19 16:57:10 +0200

    cleaned up KDTrack class, removed useless member variables and functions

--- include/KDTrack.h
+++ include/KDTrack.h
@@ -7,93 +7,83 @@
 
 // ------------------------------------------------------------------------------------
 // The KDTrack class is a simple track class designed to allow fast linear fitting in
-// conformal space. As the errors in the u (global x) direction cannot be ignored in the
-// linear track fit, a minimiser function is needed that projects these errors onto the
-// v axis. The class holds a vector of KDCluster objects, the hits attached to this track
+// conformal space. The class holds a vector of KDCluster objects (the hits attached to
+// the track) and a psuedo-kalman filter object.
 // ------------------------------------------------------------------------------------
 
 class KalmanTrack;
 
 class KDTrack{
 public:
-  
-  // Constructor
-  KDTrack();
-  
-  // Destructor
-  virtual ~KDTrack();
-  
-  // Functions to add clusters
-  void add(KDCluster* cluster){m_clusters.push_back(cluster); m_nPoints++;}
-  void insert(KDCluster* cluster){m_clusters.insert(m_clusters.begin(),cluster); m_nPoints++;}
-  void remove(int clusterN){m_clusters.erase(m_clusters.begin()+clusterN); m_nPoints--;}
-  
-		// Fit functions
-  void fit();
-  double calculateChi2(bool setErrors=false);
-  double calculateChi2SZ(TH2F* histo = NULL, bool debug = false);
-  double deltaChi2(KDCluster*);
-  void linearRegression();
-  void linearRegressionConformal(bool debug = false);
-  double sinc(double);
-  
-  // Minuit interface functions
-  double operator() (const double *x);
-  
-  // Functions to set member variables
-  void setGradient(double gradient){m_gradient = gradient;}
-  void setIntercept(double intercept){m_intercept = intercept;}
-  void setGradientError(double gradientError){m_gradientError = gradientError;}
-  void setInterceptError(double interceptError){m_interceptError = interceptError;}
-  void setGradientZS(double gradientZS){m_gradientZS = gradientZS;}
-  void setInterceptZS(double interceptZS){m_interceptZS = interceptZS;}
-  void setConformalFit(bool fit){m_conformalFit = fit;}
-  void setKalmanTrack(KalmanTrack* track){m_kalmanTrack = track;}
-  
-  // Functions to return member variables
-  double chi2ndof(){return m_chi2ndof;}
-  double chi2(){return m_chi2;}
-  double chi2ndofZS(){return m_chi2ndofZS;}
-  double chi2ZS(){return m_chi2ZS;}
-  bool rotated(){return m_rotated;}
-  std::vector<KDCluster*> clusters(){return m_clusters;}
-  double gradient(){return m_gradient;}
-  double quadratic(){return m_quadratic;}
-  double intercept(){return m_intercept;}
-  double gradientZS(){return m_gradientZS;}
-  double interceptZS(){return m_interceptZS;}
-  int nPoints(){return m_nPoints;}
-  KalmanTrack* kalmanTrack(){return m_kalmanTrack;}
+    
+    //--- Constructor and destructor
+    KDTrack();
+    virtual ~KDTrack();
+    
+    //--- Functions to add and remove clusters
+    void add(KDCluster* cluster){m_clusters.push_back(cluster); m_nPoints++;}
+    void insert(KDCluster* cluster){m_clusters.insert(m_clusters.begin(),cluster); m_nPoints++;}
+    void remove(int clusterN){m_clusters.erase(m_clusters.begin()+clusterN); m_nPoints--;}
+    
+    //--- Fit functions
+    double calculateChi2(bool setErrors=false);
+    double calculateChi2SZ(TH2F* histo = NULL, bool debug = false);
+    void linearRegression();
+    void linearRegressionConformal(bool debug = false);
+    double sinc(double);
+    void FillDistribution(TH2F*);
+    
+    //--- Functions to set and return member variables
+    
+    // UV fit parameters
+    double intercept(){return m_intercept;}
+    double gradient(){return m_gradient;}
+    double quadratic(){return m_quadratic;}
+    double chi2(){return m_chi2;}
+    double chi2ndof(){return m_chi2ndof;}
+    bool rotated(){return m_rotated;}
 
-  void FillDistribution(TH2F*);
-  
-  std::vector<KDCluster*> m_clusters;
-  bool m_conformalFit;
-  KalmanTrack* m_kalmanTrack;
+    // SZ fit parameters
+    double interceptZS(){return m_interceptZS;}
+    double gradientZS(){return m_gradientZS;}
+	double chi2ZS(){return m_chi2ZS;}
+    double chi2ndofZS(){return m_chi2ndofZS;}
 
-//private:
-  
-  // Each KDTrack contains a list of clusters, gradient and intercept
-  double m_gradient;
-  double m_intercept;
-  double m_quadratic;
-  double m_chi2;
-  double m_chi2ZS;
-  double m_ndof;
-  double m_chi2ndof;
-  double m_chi2ndofZS;
-  int m_nPoints;
-  double m_gradientZS;
-  double m_interceptZS;
-  double m_gradientError;
-  double m_interceptError;
-  bool m_rotated;
-  bool m_rotatedSZ;
-//  bool m_conformalFit;
-  bool fillFit;
-  
+    // Clusters and kalman track pointer
+    int nPoints(){return m_nPoints;}
+    std::vector<KDCluster*> clusters(){return m_clusters;}
+    KalmanTrack* kalmanTrack(){return m_kalmanTrack;}
+    void setKalmanTrack(KalmanTrack* track){m_kalmanTrack = track;}
+    
+    //--- Each KDTrack contains parameters for the two separate fits,
+    //--- along with the errors and list of clusters used
+    
+    // UV fit parameters
+    double m_gradient;
+    double m_gradientError;
+    double m_intercept;
+    double m_interceptError;
+    double m_quadratic;
+    double m_chi2;
+    double m_chi2ndof;
+    bool m_rotated;
+    
+    // SZ fit parameters
+    double m_gradientZS;
+    double m_interceptZS;
+    double m_chi2ZS;
+    double m_chi2ndofZS;
+    bool m_rotatedSZ;
+    bool fillFit;
+
+    // Clusters and kalman track pointer
+    double m_ndof;
+    int m_nPoints;
+    std::vector<KDCluster*> m_clusters;
+    KalmanTrack* m_kalmanTrack;
+    
 private:
-
+    
 };
 
 #endif
