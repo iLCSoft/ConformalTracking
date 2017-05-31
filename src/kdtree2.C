35e05641fb9468cd6694150b9420b16ba44cdc5d
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-05-31 11:28:27 +0200

    replace cout by streamlog_out(DEBUG)

--- src/kdtree2.C
+++ src/kdtree2.C
@@ -6,6 +6,7 @@
 
 #include "kdtree2.h"
 
+#include <streamlog/streamlog.h>
 #include <algorithm>
 #include <iostream>
 
@@ -614,11 +615,11 @@ namespace kdtree2 {
 
     if (debug) {
       printf("Processing terminal node %d, %d\n", l, u);
-      std::cout << "Query vector = [";
+      streamlog_out(DEBUG) << "Query vector = [";
       for (int i = 0; i < dim; i++)
-        std::cout << sr.qv[i] << ',';
-      std::cout << "]\n";
-      std::cout << "nn = " << nn << '\n';
+        streamlog_out(DEBUG) << sr.qv[i] << ',';
+      streamlog_out(DEBUG) << "]\n";
+      streamlog_out(DEBUG) << "nn = " << nn << '\n';
       check_query_in_bound(sr);
     }
 
@@ -681,13 +682,13 @@ namespace kdtree2 {
         e.dis = dis;
         sr.result.push_element_and_heapify(e);
         if (debug)
-          std::cout << "unilaterally pushed dis=" << dis;
+          streamlog_out(DEBUG) << "unilaterally pushed dis=" << dis;
         if (sr.result.size() == nn)
           ballsize = sr.result.max_value();
         // Set the ball radius to the largest on the list (maximum priority).
         if (debug) {
-          std::cout << " ballsize = " << ballsize << "\n";
-          std::cout << "sr.result.size() = " << sr.result.size() << '\n';
+          streamlog_out(DEBUG) << " ballsize = " << ballsize << "\n";
+          streamlog_out(DEBUG) << "sr.result.size() = " << sr.result.size() << '\n';
         }
       } else {
         //
@@ -700,7 +701,7 @@ namespace kdtree2 {
         e.dis    = dis;
         ballsize = sr.result.replace_maxpri_elt_return_new_maxpri(e);
         if (debug) {
-          std::cout << "Replaced maximum dis with dis=" << dis << " new ballsize =" << ballsize << '\n';
+          streamlog_out(DEBUG) << "Replaced maximum dis with dis=" << dis << " new ballsize =" << ballsize << '\n';
         }
       }
     }  // main loop
